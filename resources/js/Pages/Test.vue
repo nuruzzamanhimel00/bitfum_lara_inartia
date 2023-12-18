baaeb568413e25f6d2c5544815db26e68c6be66b
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-12-18 17:22:59 +0600

    done

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Test Page</h1>
+        <p>This is a test page</p>
+        <button @click.prevent="incrementHandler">+</button>
+        {{ count }}
+        <button @click.prevent="decrementHandler">-</button>
+    </div>
+</template>
+
+<script>
+    import { ref } from 'vue'
+    export default {
+        name:'Test',
+        setup(){
+            const count = ref(0)
+
+            function incrementHandler(){
+                count.value++
+            }
+            function decrementHandler(){
+                count.value--
+            }
+            return {
+                count,
+                incrementHandler,
+                decrementHandler
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
